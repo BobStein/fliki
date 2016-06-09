497c7da27f6475f9f9eef2efd2709be8086996ac
Bob Stein <me@bobste.in>
2016-06-09 03:39:38 -0400

    Flash_login works! But still some problem with user idn being doubly (nested) suffixed.

--- fliki.py
+++ fliki.py
@@ -1,3 +1,7 @@
+"""
+fliki is a qiki implemented in Flask and Python
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -11,6 +15,11 @@ import authomatic.adapters
 import authomatic.core
 import authomatic.providers.oauth2
 import flask   # , send_from_directory
+import flask_login
+# import flask.ext.login -- ExtDeprecationWarning: Importing flask.ext.login is deprecated, use flask_login instead.
+# SEE:  http://flask.pocoo.org/docs/0.11/extensiondev/#ext-import-transition
+import six
+import six.moves.urllib as urllib
 
 import qiki
 import secure.credentials
@@ -33,12 +42,12 @@ logger.addHandler(ch)
 # THANKS:  Log to stdout, http://stackoverflow.com/a/14058475/673991
 # logger.debug("Hi ho")
 
-flask_app_global = flask.Flask(
+flask_app = flask.Flask(
     __name__,
     static_url_path='/meta/static',
     static_folder='../qiki-javascript/'
 )
-flask_app_global.secret_key = secure.credentials.flask_secret_key
+flask_app.secret_key = secure.credentials.flask_secret_key
 
 lex = qiki.LexMySQL(**secure.credentials.for_fliki_lex_database)
 path = lex.noun(u'path')
@@ -67,6 +76,58 @@ authomatic_global = authomatic.Authomatic(
 )
 STALE_LOGIN_ERROR = 'Unable to retrieve stored state!'
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(flask_app)
+
+
+iconify_word = lex.noun(u'iconify')
+name_word = lex.noun(u'name')
+
+
+class MyUser(flask_login.UserMixin, qiki.Listing):
+
+    def lookup(self, index, callback):
+        # HACK:  Validate!!
+        namings = self.meta_word.lex.find_words(sbj=self.meta_word.lex, vrb=name_word, obj=self.idn)
+        try:
+            latest_naming = namings[0]
+        except IndexError:
+            the_name = u"(unknown {})".format(self.idn)
+            # raise RuntimeError("HACK")
+        else:
+            the_name = latest_naming.txt
+        print("find sbj", self.meta_word.lex, "vrb", name_word, "obj", self.idn, "==>", repr(namings), "-", the_name)
+        callback(the_name, qiki.Number(1))
+
+    def get_id(self):
+        return self.idn.qstring()
+
+    def get_name(self):
+        return self.txt
+
+    def get_provider_user_id(self):
+        return six.text_type(int(self.idn.get_suffix(qiki.Number.Suffix.TYPE_LISTING).payload_number()))
+
+
+listing = lex.noun(u'listing')
+# qiki.Listing.install(listing)   # Silly to do this, right?
+my_user = lex.define(listing, u'my user')
+MyUser.install(my_user)
+
+
+@login_manager.user_loader
+def user_loader(id_):
+    print("user_loader", repr(id_))
+    try:
+        user = MyUser(qiki.Number(id_))   # id_ is not the user's Listing index
+        # user = lex[id_]   # id_ is the Listing idn
+        print("\ttype", type(user).__name__)
+    except qiki.Listing.NotFound:
+        return None
+    else:
+        # user.id = id_
+        return user
+
 
 def referrer(request):
     this_referrer = request.referrer
@@ -76,21 +137,67 @@ def referrer(request):
         return qiki.Text.decode_if_you_must(this_referrer)
 
 
-@flask_app_global.route(u'/meta/play', methods=('GET', 'POST'))
+@flask_app.route(u'/meta/play', methods=('GET', 'POST'))
 def play():
+    user = flask_login.current_user
+    # print("Current user", type(user).__name__)   # Always LocalProxy
+    if user.is_authenticated:
+        log_link = """
+            <a href='{logout_url}'>
+                logout
+            </a>
+            as {user_name}
+            (local id {user_id})
+            (provider's id {provider_user_id})
+        """.format(
+            logout_url=flask.url_for('logout'),
+            user_id=user.get_id(),
+            user_name=user.get_name(),
+            provider_user_id=user.get_provider_user_id(),
+        )
+    else:
+        log_link = """
+            <a href='{login_url}'>login</a>
+        """.format(
+            login_url=flask.url_for('login')
+        )
+    return """
+        {log_link}
+    """.format(
+        log_link=log_link,
+    )
+
+
+@flask_app.route(u'/meta/logout', methods=('GET', 'POST'))
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return flask.redirect(flask.url_for('play'))
+
+
+@flask_app.route(u'/meta/login', methods=('GET', 'POST'))
+def login():
     response = flask.make_response(" Play ")
     login_result = authomatic_global.login(
         authomatic.adapters.WerkzeugAdapter(flask.request, response),
         GOOGLE_PROVIDER,
-        # The following don't help persist the logged-in condition,
-        # they just rejigger the ad hoc session supporting the banter with the provider.
+        # The following don't help persist the logged-in condition, duh,
+        # they just rejigger the brief, ad hoc session supporting the banter with the provider.
         # session=flask.session,
-        # session_saver=lambda: flask_app_global.save_session(flask.session, response),
+        # session_saver=lambda: flask_app.save_session(flask.session, response),
     )
-    print(repr(login_result))
+    # print(repr(login_result))
     if login_result:
         if hasattr(login_result, 'error') and login_result.error is not None:
             print("Login error:", str(login_result.error))
+            # Example login_result.error:
+            #
+            # Failed to obtain OAuth 2.0 access token from https://accounts.google.com/o/oauth2/token!
+            # HTTP status: 400, message: {
+            #   "error" : "invalid_grant",
+            #   "error_description" : "Invalid code."
+            # }.
+            #
             url_has_question_mark_parameters = flask.request.path != flask.request.full_path
             is_stale = str(login_result.error) == STALE_LOGIN_ERROR
             if is_stale and url_has_question_mark_parameters:
@@ -107,36 +214,53 @@ def play():
                 response.set_data("Whoops")
         else:
             if hasattr(login_result, 'user') and login_result.user is not None:
-                user = login_result.user
+                # user = login_result.user
+                login_result.user.update()
+                user = MyUser(qiki.Number(login_result.user.id))
+                # user.authomatic_user = login_result.user
                 # print("\tuser before update", repr(user))
                 # print("\tuser data before update", repr(user.data))
                 # user_data_before_update = json.dumps(user.data, indent=4)
-                user.update()
                 # print("\tuser  after update", repr(user))
                 # print("\tuser data  after update", repr(user.data))
-                response.set_data(
-                    """
-                        <p>
-                            Hello
-                            <img src='{url}'>
-                            {name} of {provider}.
-                            Your id is {id}.
-                        </p>
+                # user.id = login_result.user.id
+                picture_parts = urllib.parse.urlsplit(login_result.user.picture)
+                picture_dict = urllib.parse.parse_qs(picture_parts.query)
+                # THANKS:  For parsing URL query by name, http://stackoverflow.com/a/21584580/673991
+                picture_size_string = picture_dict.get('sz', ['0'])[0]
+                avatar_width = qiki.Number(picture_size_string)
+                avatar_url = login_result.user.picture
+                display_name = login_result.user.name
+                print("Logging in", user.index, user.idn)
+                lex[lex](iconify_word, use_already=True)[user.idn] = avatar_width, avatar_url
+                lex[lex](name_word, use_already=True)[user.idn] = display_name
+                # print("Picture size", picture_size_string)
+                flask_login.login_user(user)
+                return flask.redirect(flask.url_for('play'))
 
-                        <!-- pre>{user_dictionary}</pre -->
-                        <!-- pre>{provider_dictionary}</pre -->
-                        <!-- pre>{config_dictionary}</pre -->
-                    """.format(
-                        name=user.name,
-                        provider=login_result.provider.name,
-                        url=user.picture,
-                        id=qiki.Number(user.id).qstring(),
-                        user_dictionary=json.dumps(user.to_dict(), indent=4),
-                        # user_dictionary=user_data_before_update,
-                        provider_dictionary=json.dumps(login_result.provider.to_dict(), indent=4),
-                        config_dictionary="\n".join(config_generator(flask_app_global.config)),
-                    )
-                )
+                # response.set_data(
+                #     """
+                #         <p>
+                #             Hello
+                #             <img src='{url}'>
+                #             {name} of {provider}.
+                #             Your id is {id}.
+                #         </p>
+                #
+                #         <!-- pre>{user_dictionary}</pre -->
+                #         <!-- pre>{provider_dictionary}</pre -->
+                #         <!-- pre>{config_dictionary}</pre -->
+                #     """.format(
+                #         name=user.name,
+                #         provider=login_result.provider.name,
+                #         url=user.picture,
+                #         id=qiki.Number(user.id).qstring(),
+                #         user_dictionary=json.dumps(user.to_dict(), indent=4),
+                #         # user_dictionary=user_data_before_update,
+                #         provider_dictionary=json.dumps(login_result.provider.to_dict(), indent=4),
+                #         config_dictionary="\n".join(config_generator(flask_app.config)),
+                #     )
+                # )
                 # HACK:  Remove all this radioactive information -- HTML comments are not hid enough.
             else:
                 print("No user!")
@@ -152,13 +276,12 @@ def play():
                 # user = authomatic.core.User(login_result.provider)
                 # user.update()
                 # print("\tuser ", repr(user))
-                # print("\tname ", str(user.name))
                 # print("\temail ", str(user.email))
                 # print("\tpicture ", str(user.picture))
             else:
                 print("No provider!")
-    else:
-        print("No result, login must be in process...")
+    # else:
+    #     print("login processing...")
 
     return response
 
@@ -181,7 +304,7 @@ def config_generator(config):
     #     )
 
 
-@flask_app_global.route(u'/meta/hello', methods=('GET', 'HEAD'))
+@flask_app.route(u'/meta/all words', methods=('GET', 'HEAD'))
 def hello_world():
     this_path = flask.request.url
     path_word = lex.define(path, this_path)
@@ -189,7 +312,7 @@ def hello_world():
     me(browse)[path_word] = 1, referrer(flask.request)
 
     words = lex.find_words()
-    logger.info("Main " + str(len(words)) + " words.")
+    logger.info("Lex has " + str(len(words)) + " words.")
     reports = []
     for word in words:
         reports.append(dict(
@@ -198,14 +321,17 @@ def hello_world():
             v=word.vrb.txt,
             o=word.obj.txt,
             t=word.txt,
-            n=word.num,
+            # n=word.num,
             xn="" if word.num == 1 else "&times;" + render_num(word.num)
         ))
-
-    return flask.render_template(
+    print("all done")
+    response = flask.render_template(
         'meta.html',
         reports=reports,
+        **config_dict
     )
+    print("rendered")
+    return response
 
     # u"""<p>Hello Worldly world!</p>
     # {reports}
@@ -215,7 +341,7 @@ def hello_world():
 
 
 # To make another static directory...
-# @flask_app_global.route('/static/<path:path>')
+# @flask_app.route('/static/<path:path>')
 # def send_static(path):
 #     pass
     # print("Hello again.")
@@ -225,8 +351,8 @@ def hello_world():
     # THANKS:  Static file response, http://stackoverflow.com/a/20648053/673991
 
 
-@flask_app_global.route(u'/<path:url_suffix>', methods=('GET', 'HEAD'))
-def send_qiki(url_suffix):
+@flask_app.route(u'/<path:url_suffix>', methods=('GET', 'HEAD'))
+def answer_qiki(url_suffix):
     this_path = lex.define(path, qiki.Text.decode_if_you_must(url_suffix))
     me(question)[this_path] = 1, referrer(flask.request)
     answers = lex.find_words(vrb=answer, obj=this_path, jbo_vrb=qoolbar.get_verbs(), idn_order='DESC', jbo_order='ASC')
@@ -268,10 +394,11 @@ def native_num(num):
     elif num.is_whole():
         return int(num)
     else:
+        # TODO:  Complex? Ludicrous? Transfinite?
         return float(num)
 
 
-@flask_app_global.route(AJAX_URL, methods=('POST',))
+@flask_app.route(AJAX_URL, methods=('POST',))
 def ajax():
     action = flask.request.form['action']
     if action == u'answer':
@@ -374,7 +501,7 @@ def invalid_response(error_message):
 
 
 if __name__ == '__main__':
-    flask_app_global.run(debug=True)
+    flask_app.run(debug=True)
 
 
 # TODO:  CSRF Protection
