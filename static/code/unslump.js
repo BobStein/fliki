f1463ebc1073d8da0c12a372e492d68de06631e2
Bob Stein <me@bobste.in>
2019-06-13 07:26:56 -0500

    reload page after entering unslump

--- static/code/unslump.js
+++ static/code/unslump.js
@@ -48,8 +48,9 @@ function js_for_unslumping(window, $, MONTY) {
                     txt: text_ump
                 },
                 function ump_done(response) {
-                    // var new_words = $.parseJSON(response.new_words);
-                    // console.log("Ump", new_words.length, new_words[0].idn, new_words[0].txt);
+                    var new_words = $.parseJSON(response.new_words);
+                    console.log("Ump", new_words.length, new_words[0].idn, new_words[0].txt.substring(0,80));
+                    qoolbar.page_reload();
                 });
         });
     });
